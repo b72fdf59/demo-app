2f4a1d0daa784574f3eb2abae7c850661456534b
Adwithya Magow <avmagow@gmail.com>
2022-07-09 21:31:43 +0800

    Fix theme

--- theme.ts
+++ theme.ts
@@ -1,10 +1,8 @@
-import blue from "@mui/material/colors/blue";
-import red from "@mui/material/colors/red";
 import { createTheme } from "@mui/material/styles";
 
 export const theme = createTheme({
   palette: {
-    primary: { main: blue[500] },
-    secondary: { main: red[500] },
+    primary: { main: "#2196f3" },
+    secondary: { main: "#f44336" },
   },
 });
