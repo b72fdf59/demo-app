048a7eae45c6981d7c92efd24f5291be8458b659
Adwithya Magow <avmagow@gmail.com>
2022-07-14 02:51:26 +0800

    Make ui for home page

--- components/logo.tsx
+++ components/logo.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 
-import { Typography } from "@mui/material";
+import { SxProps, Theme, Typography } from "@mui/material";
 import Box from "@mui/material/Box";
 
 const styles = {
@@ -21,25 +21,30 @@ type Props = {
   title?: string;
   src?: string;
   subTitle?: string;
+  sx?: SxProps<Theme>;
 };
 
 export default function Logo({
   title = "Demo App",
   subTitle,
   src = "/DEMO.svg",
+  sx: sxProps = {},
 }: Props): JSX.Element {
   return (
-    <Box sx={styles.header}>
-      <Box sx={styles.logo}>
-        <Image src={src} layout="fill" alt="Logo image" objectFit="contain" />
-      </Box>
-      <Box>
-        <Typography gutterBottom variant="h6" component="h6">
-          {title}
-        </Typography>
-        <Typography gutterBottom variant="body1">
-          {subTitle}
-        </Typography>
+    <Box sx={sxProps}>
+      {/* cannot combine the box above and box below because of weird ts types */}
+      <Box sx={styles.header}>
+        <Box sx={styles.logo}>
+          <Image src={src} layout="fill" alt="Logo image" objectFit="contain" />
+        </Box>
+        <Box>
+          <Typography gutterBottom variant="h6" component="h6">
+            {title}
+          </Typography>
+          <Typography gutterBottom variant="body1">
+            {subTitle}
+          </Typography>
+        </Box>
       </Box>
     </Box>
   );
--- pages/home.tsx
+++ pages/home.tsx
@@ -1,7 +1,63 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
 import Container from "@mui/material/Container";
+import Stack from "@mui/material/Stack";
+import Paper from "@mui/material/Paper";
+import Typography from "@mui/material/Typography";
+
+import Logo from "../components/logo";
+
+type FlowCardProps = {
+  title: string;
+  description?: string;
+  buttonLabel?: string;
+  handleClick?: () => void;
+};
+const FlowCard = ({
+  title,
+  description,
+  buttonLabel = "Submit",
+  handleClick,
+}: FlowCardProps): JSX.Element => {
+  return (
+    <Paper
+      variant="outlined"
+      sx={{
+        p: 3,
+        pt: 4,
+        pb: 4,
+        width: "100%",
+        maxWidth: 500,
+        height: 200,
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between",
+        alignItems: "center",
+      }}
+      elevation={0}
+    >
+      <Box>
+        <Typography
+          align="center"
+          variant="h6"
+          gutterBottom
+          sx={{ fontWeight: "semi-bold" }}
+        >
+          {title}
+        </Typography>
+        <Typography variant="body2" color="text.secondary" align="center">
+          {description}
+        </Typography>
+      </Box>
+      <Button variant="contained" onClick={handleClick} disabled={!handleClick}>
+        {buttonLabel}
+      </Button>
+    </Paper>
+  );
+};
 
 type Props = {
   authorized: boolean;
@@ -13,11 +69,22 @@ const Home: NextPage<Props> = ({ authorized }: Props) => {
       <Head>
         <title> Finverse Demo App </title>
       </Head>
+      <Box />
       <Container maxWidth="lg">
-        <h1> {authorized ? "Authed" : "Not Authed"} </h1>
+        <Logo sx={{ marginBottom: 3 }} />
+        <Stack
+          gap={2}
+          direction="column"
+          sx={{ alignItems: "center", marginTop: 2 }}
+        >
+          <FlowCard
+            title="Data API"
+            description="Link an account and access real-time financial data"
+            buttonLabel="Link a bank account"
+          />
+        </Stack>
       </Container>
     </>
   );
 };
-
 export default Home;
